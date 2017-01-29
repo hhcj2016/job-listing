d02874c4aab3c416db6198b809b25fd080e84b1d
Cindy <cindyjiang88@163.com>
2017-01-29 17:55:52 +0800

    fix logto admin/jobs with error msg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
 
   def require_is_admin
-    if !current_user.admin?
+    if !current_user.try(:admin?)
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
